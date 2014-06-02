09573115f9ee7dfecdc83a288056fba3b5b9d9e0
FOCUS <964590970@qq.com>
2014-06-02 11:49:03 +0800

    文章查看页面

---
+++ app/views/admin/viewPost.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layout')
+@section('content')
+<div id="main-content">
+	<div class="row">
+		<div class="col-lg-12">
+			<h1>Blank Page <small>Blank Page</small></h1>
+			<br><br><br>
+		</div>
+	</div>
+</div>
+@stop
